b72fbd9c4a6ab4a4216779c5950db6c1a5f2d5b0
anandson47 <anandson47@gmail.com>
2020-08-13 00:15:45 +0530

    Updates

---
+++ precache-manifest.4cc89baf13ac3d1a886dc014e72d4c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a70152f83f0b8530a3324c3d653f227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504c26904266531c8e1b",
+    "url": "/static/css/2.306a6dee.chunk.css"
+  },
+  {
+    "revision": "7355d3e96b1d24ffaf24",
+    "url": "/static/css/main.a6ad11fe.chunk.css"
+  },
+  {
+    "revision": "504c26904266531c8e1b",
+    "url": "/static/js/2.1bdccbef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1bdccbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7355d3e96b1d24ffaf24",
+    "url": "/static/js/main.f9671d66.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
